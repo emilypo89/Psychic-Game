af15e2ef025e06206d833ff98e3f4980d238a170
emilypo89 <emilypo89@gmail.com>
2017-05-24 20:02:23 -0400

    guess tracker'

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var totalGuess = 9;
+var guessCount = 0;
+var displayGuess = [];
+  
+
+document.onkeyup = function(event) {
+  var userGuess = event.key;
+  var computerGuess = options[Math.floor(Math.random() * options.length)];
+    displayGuess.push(userGuess);
+
+
+if (userGuess === computerGuess){
+  wins++;
+  totalGuess--;
+  guessCount++;
+}
+
+else if (userGuess !== computerGuess){
+  totalGuess--;
+  guessCount++;
+  // userGuess.push(displayGuess);
+}    
+
+if (totalGuess < 0) {
+  losses++;
+  guessCount;
+  totalGuess;
+}
+
+
+
+
+
+var updateWins = '<p>Wins: ' + wins +'</p>';
+var updateLosses = '<p>Losses: ' + losses +'</p>';
+var updateTotalGuess = '<p>Number of Guesses Left: ' + totalGuess +'</p>';
+var updateGuessCount = '<p>Number of Guesses so far: ' + guessCount + '</p>';
+var updateDisplayGuess = '<p> Guessed letters: ' + displayGuess + '</p>';
+
+
+var gameDiv = document.getElementById("game");
+
+gameDiv.innerHTML = updateWins + updateLosses + updateTotalGuess + updateGuessCount + updateDisplayGuess;
+
+
+}
+     
